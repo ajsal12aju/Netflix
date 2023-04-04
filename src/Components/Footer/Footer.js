aead8c0221390cb17bd4ab2d8328b2fa15e99545
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-04-04 22:46:23 +0530

    footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             <div class="card w-100 bg-transparent border-white">
              <div class="card-body d-flex align-items-center">
                 <div className='text-white'>
-                 <h5 class="card-title rem-2">Card title</h5>
+                 <h5 class="card-title rem-2">Customise your home screen</h5>
                  <p className='w-75 card-text' >With supporting text below as a natural lead-in to additional content.</p>
                  </div>
                  <div className='d-flex align-items-center justify-content-center'>
