f606d86624b85028e4581b491fc8918ec3e8a738
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-04-04 22:24:10 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import {originals,action} from './urls'
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
      <Banner/>
      <RowPost url={originals} title="Netflix Originals"/>
      <RowPost url={action} title="Action" isSmall/>
+     <Footer/>
 
     </div>
   );
